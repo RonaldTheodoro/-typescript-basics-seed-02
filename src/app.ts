acae2ac70f3fd62544e551401e03b71bbb48d947
Ronald Theodoro <ronaldtheodoro@gmail.com>
2020-08-22 00:22:06 -0300

    The “Any” Type

--- src/app.ts
+++ src/app.ts
@@ -1,11 +1,7 @@
-const pizzas: number = 3;
+let coupon: any;
 
-function offerDiscount(orders: number): boolean {
-  return orders >= 3;
-}
+coupon = 'PIZZA25';
+console.log(coupon);
 
-if (offerDiscount(pizzas)) {
-  console.log('You are entitled to a discount');
-} else {
-  console.log('Order more than 3 pizzas for a discount!');
-}
+coupon = 25;
+console.log(coupon);
