a8a7ac4c5ca0b1de6400f6e9e03843ed3111dc4b
Ronald Theodoro <ronaldtheodoro@gmail.com>
2020-08-23 01:37:22 -0300

    Setters and Getters (Accessors)

--- src/app.ts
+++ src/app.ts
@@ -1,3 +1,22 @@
+// eslint-disable-next-line max-classes-per-file
+class Sizes {
+  constructor(public sizes: string[]) { }
+
+  set availableSizes(sizes: string[]) {
+    this.sizes = sizes;
+  }
+
+  get availableSizes(): string[] {
+    return this.sizes;
+  }
+}
+
+const sizes = new Sizes(['small', 'medium']);
+
+console.log(sizes.availableSizes);
+sizes.availableSizes = ['medium', 'large'];
+console.log(sizes.availableSizes);
+
 class Pizza {
   toppings: string[] = [];
 
