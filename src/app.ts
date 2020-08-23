6a83ddcaf69d5d3570a2117d006c540bce344ac1
Ronald Theodoro <ronaldtheodoro@gmail.com>
2020-08-23 00:48:26 -0300

    Interfaces and Optional Properties

--- src/app.ts
+++ src/app.ts
@@ -4,21 +4,23 @@ interface Sizes {
 
 interface Pizza extends Sizes {
   name: string;
+  toppings?: number;
   getAvailableSizes(): string[];
 }
 
 let pizza: Pizza;
 
-function createPizza(name: string, sizes: string[]): Pizza {
+function createPizza(name: string, sizes: string[], toppings?: number): Pizza {
   return {
     name,
     sizes,
+    toppings,
     getAvailableSizes() {
       return this.sizes;
     },
   };
 }
 
-pizza = createPizza('Pepperoni', ['small', 'medium']);
+pizza = createPizza('Pepperoni', ['small', 'medium'], 1);
 
 console.log(pizza);
