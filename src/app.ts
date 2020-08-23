398d2e9ffcbbf96d118ed1ecf1d98b9cec085790
Ronald Theodoro <ronaldtheodoro@gmail.com>
2020-08-23 00:22:36 -0300

    Interfaces with Function Types

--- src/app.ts
+++ src/app.ts
@@ -1,12 +1,19 @@
 interface Pizza {
   name: string;
   sizes: string[];
+  getAvailableSizes(): string[];
 }
 
 let pizza: Pizza;
 
 function createPizza(name: string, sizes: string[]): Pizza {
-  return { name, sizes };
+  return {
+    name,
+    sizes,
+    getAvailableSizes() {
+      return this.sizes;
+    },
+  };
 }
 
 pizza = createPizza('Pepperoni', ['small', 'medium']);
